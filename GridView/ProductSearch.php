e639200d70e623fa56204990dd4db2e8b3c28435
SvetaMor <acura88@yandex.ru>
2019-07-07 22:49:26 +0300

    Made some changes

--- GridView/ProductSearch.php
+++ GridView/ProductSearch.php
@@ -20,8 +20,27 @@ class ProductSearch extends Model
          ];
    }
    
-   public function search($param,$arr)
+   public function search($param)
    {
+      $models = [];
+      $colomn = 'categoryId';
+      $colomnSub = 'name';
+      $atr = 'item';
+      $filename =  \Yii::getAlias('@webroot').'/files/products.xml';
+      $filenameSub = \Yii::getAlias('@webroot').'/files/categories.xml';
+      $fileObject = simplexml_load_file($filename);
+      $fileObjectSub = simplexml_load_file($filenameSub);
+      $jsonString = json_encode($fileObject);
+      $resultArray = json_decode($jsonString, TRUE); 
+      $jsonStringSub = json_encode($fileObjectSub);
+      $resultArraySub = json_decode($jsonStringSub, TRUE);
+      $limit = $fileObject->count();
+      for ($count = 0; $count < $limit; ++$count) {
+         $models[] = $resultArray[$atr][$count];
+         $num = $resultArray[$atr][$count][$colomn]-1;
+         $models[$count][$colomn] = $resultArraySub[$atr][$num][$colomnSub];
+      }
+      
       $jsonStr = json_encode($param);
       $resultArray = json_decode($jsonStr, TRUE);
       
@@ -29,29 +48,25 @@ class ProductSearch extends Model
       if (isset($resultArray["ProductSearch"])) {
         
          $filter = array_filter($resultArray["ProductSearch"], function($v){
-            return ($v != "");
-         });
-        
+             return ($v != "");
+            });
          
          $key = array_keys($filter)[0];
          $value = $filter[$key];
          
-         $limit = count($arr);
+         $limit = count($models);
          for ($count = 0; $count < $limit; ++$count) {
-            $values = array_filter($arr[$count], function($v, $k) use ($key,$value) {
+            $values = array_filter($models[$count], function($v, $k) use ($key,$value) {
                return ($k == $key and $v == $value);
             }, ARRAY_FILTER_USE_BOTH);
-            
             if (count($values) != 0)
-               $newArr[] = $arr[$count];
+               $newArr[] = $models[$count];
          }
-         
-         $arr = $newArr;
-         
+        $models =  $newArr;
       }
       
       $dataProvider = new ArrayDataProvider([
-          'allModels' => $arr,
+          'allModels' => $models,
           'pagination' => [
               'pageSize' => 5,
           ],
